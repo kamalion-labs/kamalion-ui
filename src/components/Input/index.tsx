365caf62ec8cb99845d6a573a404f3a4212e9746
Rony Moura <ronymmoura@gmail.com>
2022-08-29 22:52:35 -0300

    Fixing input value

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import NumberFormat from 'react-number-format';
+import NumberFormat, { NumberFormatValues, SourceInfo } from 'react-number-format';
 
 export interface InputProps {
   value: any;
-  onChange: any;
+  onChange: (values: NumberFormatValues, sourceInfo?: SourceInfo) => void;
 
   maskType?: 'text' | 'tel' | 'cel' | 'money' | 'percent' | 'date';
   className?: string;
 
   name?: string;
   id?: string;
+  max?: number;
 }
 
-export const Input: React.FC<InputProps> = ({ id, name, value, onChange, maskType = 'text', className, ...rest }) => {
+export const Input: React.FC<InputProps> = ({ id, name, value, onChange, max, maskType = 'text', className, ...rest }) => {
   const maskProps = {
     money: {
       decimalScale: 2,
@@ -42,8 +43,25 @@ export const Input: React.FC<InputProps> = ({ id, name, value, onChange, maskTyp
   };
 
   if (maskType !== 'text' && Object.keys(maskProps).includes(maskType)) {
-    return <NumberFormat className={`input ${className}`} value={value} onValueChange={(e: any) => onChange(e)} {...maskProps[maskType]} {...rest} />;
+    return (
+      <NumberFormat className={`input ${className}`} value={value} onValueChange={onChange} maxLength={max} {...maskProps[maskType]} {...rest} />
+    );
   }
 
-  return <input type={maskType} className={`input ${className}`} onChange={(e) => onChange(e.target.value)} value={value} {...rest} />;
+  return (
+    <input
+      type={maskType}
+      className={`input ${className}`}
+      onChange={(e) =>
+        onChange({
+          value: e.target.value,
+          floatValue: undefined,
+          formattedValue: e.target.value
+        })
+      }
+      value={value}
+      maxLength={max}
+      {...rest}
+    />
+  );
 };
