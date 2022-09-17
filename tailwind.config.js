2f68fbb7282c6567a003174864c561f8b3e6d5d9
Rony Moura <ronymmoura@gmail.com>
2022-09-17 00:29:34 -0300

    Fixing color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'color-primary': 'var(--color-primary)',
+        'color-secondary': 'var(--color-secondary)',
+        'color-warn': 'var(--color-warn)',
+        'color-danger': 'var(--color-danger)',
+        'color-info': 'var(--color-info)',
+        'color-success': 'var(--color-success)'
+      }
+    }
   },
   plugins: [require('postcss-import')]
 };
