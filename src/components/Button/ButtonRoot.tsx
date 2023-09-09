897ca12b0e04f2d997e2f2ec7c2fba8a2e5efc30
Rony Moura <ronymmoura@gmail.com>
2023-09-09 20:14:10 -0300

    Fix button props

--- src/components/Button/ButtonRoot.tsx
+++ src/components/Button/ButtonRoot.tsx
@@ -4,28 +4,34 @@ import { type VariantProps } from "class-variance-authority";
 import { ButtonVariants } from "./ButtonVariants";
 import { cn } from "../../util/cn";
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof ButtonVariants> {}
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof ButtonVariants>;
 
-const ButtonRoot = ({
-  variant,
-  className,
-  type = "button",
-  children,
-  size,
-  ...props
-}: ButtonProps) => {
-  return (
-    <button
-      className={cn(ButtonVariants({ variant, size, className }))}
-      type={type}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+const ButtonRoot = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant,
+      className,
+      type = "button",
+      children,
+      size,
+      ...props
+    }: ButtonProps,
+    ref
+  ) => {
+    return (
+      <button
+        className={cn(ButtonVariants({ variant, size, className }))}
+        type={type}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
 ButtonRoot.displayName = "ButtonRoot";
 
 export { ButtonRoot };
