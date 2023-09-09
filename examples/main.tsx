2151a2d227359e3e874f24c37ac7d2f3af264445
Rony Moura <ronymmoura@gmail.com>
2023-09-09 19:55:57 -0300

    Fix styles

--- examples/main.tsx
+++ examples/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import "./styles/global.css";
+import "../dist/styles.css";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
