c59dc83b85961f56c0b5ca4fab17ce2d4a02a857
Rony Moura <ronymmoura@gmail.com>
2023-09-10 01:36:13 -0300

    Fix theming

--- apps/examples/src/main.tsx
+++ apps/examples/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import "@kamalion/ui/dist/styles.css";
+import "./styles/global.css";
+
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- packages/kamalion-ui/tailwind.config.js
+++ packages/kamalion-ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./examples/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
