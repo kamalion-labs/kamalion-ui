78339de2817ba83b601741fc2d00fc3618ef5e8c
Rony Moura <ronymmoura@gmail.com>
2024-01-05 06:26:24 -0300

    fix: progressbar radix reference

---
+++ packages/kamalion-ui/src/components/Skeleton/index.tsx
@@ -0,0 +1,15 @@
+import { cn } from "../..";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-zinc-900/10", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
